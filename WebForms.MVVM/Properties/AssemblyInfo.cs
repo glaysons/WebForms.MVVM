b522c74628818962389f388343ced9c73020a985
glaysons <glaysons@gmail.com>
2017-11-15 08:16:33 -0300

    Mudança da versão de compilação.

--- WebForms.MVVM/Properties/AssemblyInfo.cs
+++ WebForms.MVVM/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebForm.MVVM")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("WebForms MVVM")]
+[assembly: AssemblyDescription("WebForms.MVVM é um conjunto de ferramentas que permite tratar os componentes do WebForms de aplicações legadas como **ValueObjects**.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebForm.MVVM")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Glayson Sergio")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © Glayson Sergio 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
